6ff1b7545d5b5a8a8b0114b70e1a7a811a411f7d
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2026-06-25 20:28:30 +0530

    feat: enhance consultation page layout and styling

--- app/pages/consultation.vue
+++ app/pages/consultation.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h3>Kindly contact through <a href="https://www.instagram.com/galacticgeneastro/" target="_blank" rel="noopener noreferrer">Instagram</a> Pages or
+      
+        Reddit
+        
+        Profiles - 
+        <a href="https://www.reddit.com/user/Huge-Description-401/" target="_blank" rel="noopener noreferrer">Main</a> & 
+        <a href="https://www.reddit.com/user/KlutzyMap7925/" target="_blank" rel="noopener noreferrer">Secondary</a> </h3>
   </div>
 </template>
 
@@ -8,6 +14,10 @@
 
 </script>
 
-<style>
+<style scoped>
 
+a{
+  text-decoration: underline;
+  color: #7289da;
+}
 </style>
